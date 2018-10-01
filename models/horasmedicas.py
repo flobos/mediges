be431127506a2807c9a8ebab3da8213482054c3f
flobos <fernandolobos@gmail.com>
2018-10-01 00:41:57 -0300

    Horas Medicas con estados

--- models/horasmedicas.py
+++ models/horasmedicas.py
@@ -11,14 +11,19 @@ class horasmedicas(models.Model):
 
     company =  fields.Many2one('res.company', string="Hora Para", required=True,readonly=True ,default=lambda self: self.env['res.company']._company_default_get('account.invoice'))
     fecha_solicitud_hora = fields.Datetime(String="Hora Reserva", required=True)
-    fecha_solicitud_hora_termino = fields.Datetime(String="Hora Reserva")
+    fecha_solicitud_hora_termino = fields.Datetime(string="Hora Termino Reserva" , readonly=True)
     paciente = fields.Many2one('res.partner', string="Paciente", required=True)
     forma_de_pago = fields.Many2one('mediges.formas_de_pagos', string="Forma de Pagos", required=True)
     historial = fields.Text(string="Historial")
     diagnostico = fields.Text(string="Diagnostico")
     Observacion = fields.Text(string="Observacion")
     antecedentes_paciente = fields.Text(string="Antecedentes Paciente")
-
+    state = fields.Selection([
+        ('draft', "Borrador"),
+        ('confirmed', "Confirmada"),
+        ('pagada', "Pagada"),
+        ('final', "Finalizada"),
+        ], default='draft', string="Estado")
 
     @api.onchange('fecha_solicitud_hora')
     def calcula_hora_termino(self):
@@ -27,3 +32,16 @@ class horasmedicas(models.Model):
         if self.fecha_solicitud_hora != False:
             hora = fields.Datetime.from_string(self.fecha_solicitud_hora)
             self.fecha_solicitud_hora_termino = hora + timedelta(minutes=15)
+
+    @api.multi
+    def btn_confirma_hora(self):
+        self.write({'state': "confirmed"})
+
+    @api.multi
+    def btn_paga_hora(self):
+        self.write({'state': "pagada"})
+
+    @api.multi
+    def btn_finaliza_hora(self):
+        self.write({'state': "final"})
+
--- models/partner.py
+++ models/partner.py
@@ -10,8 +10,6 @@ class pacientes(models.Model):
 
     _inherit = 'res.partner'
     antecedentes_medicos = fields.Text(string="Antecedentes Medicos")
-    rut = fields.Char(String="Rut", required=True)
-    numero_celular = fields.Char(string="Celular", required=True )
-    fecha_nacimiento = fields.Date(string="Fecha Nacimiento")
+
 
 

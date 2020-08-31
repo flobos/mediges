1d397c73a6677c55848738fba3a7e6425b555fd7
Fernando Lobos <fernandolobos@gmail.com>
2020-08-30 23:13:35 -0400

    calcula icm

--- models/visitas.py
+++ models/visitas.py
@@ -1,8 +1,4 @@
 from odoo import api, fields, models, _
-from datetime import date, datetime, timedelta
-from odoo.exceptions import ValidationError
-from odoo.exceptions import UserError, AccessError
-from odoo import exceptions, _
 
 
 class visitas(models.Model):
@@ -17,11 +13,16 @@ class visitas(models.Model):
                                             ,'Se revisa Criterios de inclusión/exclusión para screening, anexo 2', required=True)
     demografia_id = fields.Many2one('mediges.demografia', string="Demografia", required=True)
     estatura = fields.Integer(string="Estatura", required=True)
-    peso = fields.Integer(string="Peso", required=True)
-    imc = fields.Integer(string="IMC", readonly=True)
+    peso = fields.Integer(string="Peso", required=True, )
+    imc = fields.Integer(string="IMC", readonly=True, compute='_calcula_icm')
     signos_vitales_id = fields.One2many('mediges.signos_vitales', 'visitas_id', string='Signos Vitales')
 
-
+    @api.multi
+    @api.depends('peso')
+    def _calcula_icm(self):
+      for record in self:
+        if record.peso and record.estatura:
+            record.imc = record.peso / ((record.estatura / 100)**2)
 
 
 

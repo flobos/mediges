ee3b7bd3d3b3259abb3ba052f0864beb2e18d5ec
Fernando Lobos <fernandolobos@gmail.com>
2020-08-31 21:43:59 -0400

    load one2many signos_vitales

--- models/visitas.py
+++ models/visitas.py
@@ -4,6 +4,22 @@ from odoo import api, fields, models, _
 class visitas(models.Model):
     _name = "mediges.visitas"
 
+    @api.model
+    def default_get(self, fields):
+        res = super(visitas, self).default_get(fields)
+        lista_signos_vitales = []
+        record_signos_vitales = self.env['mediges.tipo_signo_vital'].search([])
+        for record in record_signos_vitales:
+            lista = (0, 0, {
+                'tipo_signo_vital': record.id,
+                'valor': 0
+            })
+        lista_signos_vitales.append(lista)
+        res.update({
+            'signos_vitales_id': lista_signos_vitales
+        })
+        return res
+
     paciente = fields.Many2one('res.partner', string="Paciente", required=True)
     consentimiento = fields.Selection([('true','Si'),('false','No')]
                                       ,'Se realiza proceso de Consentimiento informado, anexo 1',required=True)

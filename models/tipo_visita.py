43ce43e27709be15b34b67d23226ca08ed3fc4c5
Fernando Lobos <fernandolobos@gmail.com>
2020-10-07 23:33:16 -0300

    fix calendar default, fix data visit

--- models/tipo_visita.py
+++ models/tipo_visita.py
@@ -3,6 +3,7 @@ from odoo import api, fields, models, _
 
 class tipo_visitas(models.Model):
     _name = "mediges.tipo_visita"
-    _order = 'id'
+    _order = 'orden'
 
     name = fields.Char(string="Tipo Vista", required=True)
+    orden = fields.Integer(string="Orden", required=True )
